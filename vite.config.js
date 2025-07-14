7704faf6d45efafb1fe6ce5141bff332d7cfdd4d
Ani-tem <anirudh.tvvb@gmail.com>
2025-07-14 23:28:15 +0530

    Fix path and ignore node_modules

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'Frontened',            // 👈 Tells Vite where to find index.html
+  root: 'Frontend',            // 👈 Tells Vite where to find index.html
   plugins: [react()],
   build: {
     outDir: '../dist',         // 👈 Optional: to avoid building inside the source folder
