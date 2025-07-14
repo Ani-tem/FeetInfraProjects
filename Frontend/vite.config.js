c2933d852d421d6e2c4d3e7a5391cccfcfe2ff44
Ani-tem <anirudh.tvvb@gmail.com>
2025-07-14 23:37:45 +0530

    Anna Dandam Pedata

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'Frontened',            // 👈 Tells Vite where to find index.html
   plugins: [react()],
   build: {
-    outDir: '../dist',         // 👈 Optional: to avoid building inside the source folder
-    emptyOutDir: true,
-  },
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
